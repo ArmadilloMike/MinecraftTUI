887f7466d028e74a2ae9b056edd662e269792a46
JacksonWengler <jacksonwengler@gmail.com>
2026-04-21 17:17:35 -0500

    added stuff to the git ignore to ignore the installtions and logs

--- main.py
+++ main.py
@@ -1,9 +1,21 @@
 import minecraft_launcher_lib
+import subprocess
+
+
+minecraft_directory = minecraft_launcher_lib.utils.get_minecraft_directory()
+versions = minecraft_launcher_lib.utils.get_available_versions(minecraft_directory)
+use_version = input("What minecraft version do you want to install? ")
 
 minecraft_directory = "installations/minecraft-1-17-1"
-minecraft_launcher_lib.install.install_minecraft_version("1.17", minecraft_directory)
+print(f"Installing minecraft at {minecraft_directory}")
+#minecraft_launcher_lib.install.install_minecraft_version("1.17", minecraft_directory)
 
-options = minecraft_launcher_lib.utils.generate_test_options()
+#options = minecraft_launcher_lib.utils.generate_test_options()
+#options["demo"] = True
 
-minecraft_command = minecraft_launcher_lib.command.get_minecraft_command("1.17", minecraft_directory, options)
-print(minecraft_command)
+#minecraft_command = minecraft_launcher_lib.command.get_minecraft_command("1.17", minecraft_directory, options)
+#print(minecraft_command)
+#subprocess.run(minecraft_command)
+
+
+
