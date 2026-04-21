777c860e98d93cff5341c8d40027ed43c1981c14
JacksonWengler <jacksonwengler@gmail.com>
2026-04-21 18:21:22 -0500

    added a util file for useful functions. I also made it so you can type in what version you want to play

--- main.py
+++ main.py
@@ -1,21 +1,31 @@
 import minecraft_launcher_lib
 import subprocess
+from mine_utils import *
 
 
 minecraft_directory = minecraft_launcher_lib.utils.get_minecraft_directory()
+
+
+# version stuff
 versions = minecraft_launcher_lib.utils.get_available_versions(minecraft_directory)
 use_version = input("What minecraft version do you want to install? ")
+version_ids = [v["id"] for v in versions]
+if use_version in version_ids:
+    print("found it!")
+else:
+    print("could not find it")
+use_version.replace(".","-")
 
-minecraft_directory = "installations/minecraft-1-17-1"
+
+# installing
+minecraft_directory = f"installations/minecraft-{use_version}"
 print(f"Installing minecraft at {minecraft_directory}")
-#minecraft_launcher_lib.install.install_minecraft_version("1.17", minecraft_directory)
+callback = show_progress()
+minecraft_launcher_lib.install.install_minecraft_version(use_version, minecraft_directory, callback=callback)
 
-#options = minecraft_launcher_lib.utils.generate_test_options()
-#options["demo"] = True
+options = minecraft_launcher_lib.utils.generate_test_options()
+options["demo"] = True
 
-#minecraft_command = minecraft_launcher_lib.command.get_minecraft_command("1.17", minecraft_directory, options)
+minecraft_command = minecraft_launcher_lib.command.get_minecraft_command(use_version, minecraft_directory, options)
 #print(minecraft_command)
-#subprocess.run(minecraft_command)
-
-
-
+subprocess.run(minecraft_command)
---
+++ mine_utils.py
@@ -0,0 +1,20 @@
+def show_progress():
+
+    current_max = 0
+    def set_status(status: str):
+        print(status)
+
+    def set_progress(progress: int):
+        if current_max != 0:
+            print(f"{progress}/{current_max}")
+
+    def set_max(new_max: int):
+        global current_max
+        current_max = new_max
+
+    callback = {
+        "setStatus": set_status,
+        "setProgress": set_progress,
+        "setMax": set_max
+    }
+    return callback
