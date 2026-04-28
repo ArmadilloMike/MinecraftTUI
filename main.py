1b215397692cad12abd40bc349df6f89d4182df9
JacksonWengler <jacksonwengler@gmail.com>
2026-04-28 17:05:38 -0500

    made it continuously ask for version until it is correct and ask if minecraft wants to be started

--- main.py
+++ main.py
@@ -10,10 +10,10 @@ minecraft_directory = minecraft_launcher_lib.utils.get_minecraft_directory()
 versions = minecraft_launcher_lib.utils.get_available_versions(minecraft_directory)
 use_version = input("What minecraft version do you want to install? ")
 version_ids = [v["id"] for v in versions]
-if use_version in version_ids:
-    print("found it!")
-else:
+while not(use_version in version_ids):
     print("could not find it")
+    use_version = input("What minecraft version do you want to install? ")
+print("found it!")
 use_version.replace(".","-")
 
 
@@ -22,10 +22,14 @@ minecraft_directory = f"installations/minecraft-{use_version}"
 print(f"Installing minecraft at {minecraft_directory}")
 callback = show_progress()
 minecraft_launcher_lib.install.install_minecraft_version(use_version, minecraft_directory, callback=callback)
+print(f"Minecraft version {use_version} installed!")
 
 options = minecraft_launcher_lib.utils.generate_test_options()
 options["demo"] = True
 
-minecraft_command = minecraft_launcher_lib.command.get_minecraft_command(use_version, minecraft_directory, options)
-#print(minecraft_command)
-subprocess.run(minecraft_command)
+
+play = input("Do you want to play minecraft now? (Y/n)").lower()
+if play == "y" or play == "":
+    minecraft_command = minecraft_launcher_lib.command.get_minecraft_command(use_version, minecraft_directory, options)
+    # print(minecraft_command)
+    subprocess.run(minecraft_command)
